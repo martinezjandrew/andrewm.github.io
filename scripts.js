f8a4a995cb5428718e3c51998aaac81c0bd0d463
andrew martinez <martinezjandrew@gmail.com>
2023-12-22 13:27:32 -0600

    javascript

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 function openNewLocalPage(url) {
   window.location.href = url;
-}      
+}
+
+function generateTopBar() {
+    const topbar = document.createElement("div");
+    topbar.className = "topline";
+    topbar.name = "topbar";
+
+    const circle = document.createElement("div");
+    circle.className = "topline circle"
+    const profilePictureImage = document.createElement("img");
+    profilePictureImage.src = "images/amongus.png"
+    profilePictureImage.alt = "picutre of andrew martinez"
+    circle.appendChild(profilePictureImage);
+
+    
+
+    
+}
